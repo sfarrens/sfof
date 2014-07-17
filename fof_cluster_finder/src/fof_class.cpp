1a272f278e1220fb341ba9a209dc1c604d3e82ac
sfarrens <samuel.farrens@googlemail.com>
2014-07-17 18:33:44 +0200

    fof_class.cpp minor update

--- fof_cluster_finder/src/fof_class.cpp
+++ fof_cluster_finder/src/fof_class.cpp
@@ -1,16 +1,17 @@
 /*Class for performing friends-of-friends*/
 
+// [Header files]
 #include "fof_class.hpp"
 
 void FoF::setup (double link_r_val, double link_z_val, const std::string &mode_val) { 
-  //! Function to set-up a FoF instance.
+  // Function to set-up a FoF instance.
   link_r = link_r_val;
   link_z = link_z_val;
   mode = mode_val;
 }
 
 bool FoF::bin_check (const Zbin &zbin, const Galaxy &gal) {
-  //! Function that checks if a galaxy is compatible with a given redshift bin.
+  // Function that checks if a galaxy is compatible with a given redshift bin.
   if (mode == "spec")
     return true;
   else
@@ -18,13 +19,13 @@ bool FoF::bin_check (const Zbin &zbin, const Galaxy &gal) {
 }
 
 bool FoF::node_check (const Galaxy &gal, const Kdtree::Kdtree_node &node, double rfriend) {
-  //! Function that checks if a galaxy is compatibile with a given kd-tree node.
+  // Function that checks if a galaxy is compatibile with a given kd-tree node.
   double dist = astro.angsep(gal.ra, gal.dec, node.ra, node.dec) - node.radius;
   return dist <= rfriend;
 }
 
 bool FoF::friendship (const Zbin &zbin, const Galaxy &gal1, const Galaxy &gal2, double rfriend) {
-  //! Function that checks if two galaxies are friends in a given redshift bin.
+  // Function that checks if two galaxies are friends in a given redshift bin.
   bool final_check;
   bool check0 = gal1.num != gal2.num;
   bool check1 = bin_check(zbin, gal2);
@@ -41,7 +42,7 @@ bool FoF::friendship (const Zbin &zbin, const Galaxy &gal1, const Galaxy &gal2, 
 }
 
 void FoF::new_cluster (const Zbin &zbin, Galaxy &gal1, Galaxy &gal2) {
-  //! Function to create a new cluster.
+  // Function to create a new cluster.
   cluster_count++;
   Cluster cluster_here(cluster_count);
   gal1.in_cluster[zbin.num] = true;
@@ -52,7 +53,7 @@ void FoF::new_cluster (const Zbin &zbin, Galaxy &gal1, Galaxy &gal2) {
 }
 
 void FoF::add_member (const Zbin &zbin, Galaxy &gal, Cluster &cluster) {
-  //! Function to add a new member to an existing cluster.
+  // Function to add a new member to an existing cluster.
   gal.in_cluster[zbin.num] = true;
   cluster.add_gal(gal);
 }
@@ -60,69 +61,69 @@ void FoF::add_member (const Zbin &zbin, Galaxy &gal, Cluster &cluster) {
 void FoF::find_friends (const Zbin &zbin, Galaxy &gal, 
 			double rfriend, std::vector<Galaxy> &gal_list,
 			const Kdtree &tree) {
-  //! Function to find galaxies linked to the galaxy in question.
-  /**< Loop through kd-tree nodes */
+  // Function to find galaxies linked to the galaxy in question.
+  /* Loop through kd-tree nodes */
   for(int j = 0; j < tree.node_list.size(); j++) {
-    /**< Check if galaxy is compatible with kd-tree node */
+    /* Check if galaxy is compatible with kd-tree node */
     if(node_check(gal, tree.node_list[j], rfriend)) {
-      /**< Loop through node members */
+      /* Loop through node members */
       for(int k = 0; k < tree.node_list[j].members.size(); k++) {
-	/**< Check if galaxies are friends */
+	/* Check if galaxies are friends */
 	int gal_now = tree.node_list[j].members[k].num;
 	if(friendship(zbin, gal, gal_list[gal_now], rfriend)) {
-	  /**< Create new cluster */
+	  /* Create new cluster */
 	  if(!gal.in_cluster[zbin.num]) 
 	    new_cluster(zbin, gal, gal_list[gal_now]);
-	  /**< Add new member to existing cluster */
+	  /* Add new member to existing cluster */
 	  else 
 	    add_member(zbin, gal_list[gal_now], list_of_clusters[cluster_count]);
 	}
-      } // end of node member loop
+      } /* end of node member loop */
     }
-  } // end of node loop
+  } /* end of node loop */
 }
 
 void FoF::find_friends_of_friends (const Zbin &zbin, Cluster &cluster, 
 				   double rfriend, std::vector<Galaxy> &gal_list, 
 				   const Kdtree &tree){
-  //! Function to find the galaxies linked to existing cluster members.
-  /**< Loop through cluster members */
+  // Function to find the galaxies linked to existing cluster members.
+  /* Loop through cluster members */
   for(int i = 0; i < cluster.mem.size(); i++) {
     find_friends(zbin, gal_list[cluster.mem[i].num], rfriend, gal_list, tree);
-  } // end of cluster member loop
+  } /* end of cluster member loop */
 }
 
 void FoF::friends_of_friends (int bin_num, const std::vector<Zbin> &zbin_list, 
 			      std::vector<Galaxy> &gal_list, const Kdtree &tree) {
-  //! Funciton find friends-of-friends in a given redshift bin.
+  // Funciton find friends-of-friends in a given redshift bin.
   cluster_count = -1;
   Zbin zbin = zbin_list[bin_num];
   double rfriend = zbin.rfriend;
-  /**< Loop through galaxies */
+  /* Loop through galaxies */
   for(int i = 0; i < gal_list.size(); i++) {
-    /**< Modify rfriend for spectroscopic mode*/
+    /* Modify rfriend for spectroscopic mode */
     if (mode == "spec") rfriend = zbin_list[gal_list[i].bin].link_r / gal_list[i].da;
-    /**< Check if galaxy is already in a cluster (f-loop)*/
+    /* Check if galaxy is already in a cluster (f-loop) */
     if(!gal_list[i].in_cluster[zbin.num] && bin_check(zbin, gal_list[i])) { 
       find_friends(zbin, gal_list[i], rfriend, gal_list, tree);
-      /**< Check if galaxy is now in a cluster (fof-loop)*/
+      /* Check if galaxy is now in a cluster (fof-loop) */
       if(gal_list[i].in_cluster[zbin.num])
 	find_friends_of_friends(zbin, list_of_clusters[cluster_count], rfriend, gal_list, tree);
     }
-  } //end of galaxy loop
+  } /* end of galaxy loop */
 }
 
 void FoF::remove (int min_ngal) {
-  //! Function that removes clusters that have too few members.
+  // Function that removes clusters that have too few members.
   std::vector<int> remove_list;
-  /**< Loop through clusters */
+  /* Loop through clusters */
   for(int i = 0; i < list_of_clusters.size(); i++) {
     list_of_clusters[i].unique();
-    /**< Check if cluster has less than the minimum number of members */
+    /* Check if cluster has less than the minimum number of members */
     if(list_of_clusters[i].mem.size() < min_ngal)
       remove_list.push_back(i);
   }
-  /**< Loop backwards through list of items to be removed */
+  /* Loop backwards through list of items to be removed */
   for(int i = remove_list.size(); i --> 0;)
     list_of_clusters.erase(list_of_clusters.begin() + remove_list[i]);
 }
