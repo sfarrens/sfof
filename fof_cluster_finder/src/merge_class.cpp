bc4d27ef6268d789377f9214381ad02a3bccdfd2
sfarrens <samuel.farrens@googlemail.com>
2014-07-18 14:42:05 +0200

    Updated comments for class: Merge

--- fof_cluster_finder/src/merge_class.cpp
+++ fof_cluster_finder/src/merge_class.cpp
@@ -3,33 +3,33 @@
 #include "merge_class.hpp"
 
 bool Merge::gal_in_clt (const Galaxy &gal, const Cluster &clt) {
-  //! Function to check if a Galaxy instance is a member of a Cluster instance.
+  // Function to check if a Galaxy instance is a member of a Cluster instance.
   return std::find(clt.mem.begin(), clt.mem.end(), gal) != clt.mem.end();
 }
 
 bool Merge::check_mem (const Cluster &clt1, const Cluster &clt2) {
-  //! Funciton to check if two Cluster instances share any Galaxy instances.
-  /**< Loop through cluster 1 members */
+  // Funciton to check if two Cluster instances share any Galaxy instances.
+  /* Loop through cluster 1 members */
   for(int i = 0; i < clt1.mem.size(); i++) 
-    /**< Check if member i is in cluster 2 */
+    /* Check if member i is in cluster 2 */
     if(gal_in_clt(clt1.mem[i], clt2))
       return true;
   return false;
 }
 
 void Merge::assimilate (Cluster &clt1, Cluster &clt2) {
-  //! Function to assimilate members from one Cluster instance into another.
-  /**< Loop through cluster 2 members */
+  // Function to assimilate members from one Cluster instance into another.
+  /* Loop through cluster 2 members */
   for(int i = 0; i < clt2.mem.size(); i++) 
-    /**< Add member i to cluster 1 */
+    /* Add member i to cluster 1 */
     clt1.add_gal(clt2.mem[i]);
-  /**< Clear contents of cluster 2 */
+  /* Clear contents of cluster 2 */
   clt2.clear();
   clt1.unique();
 }
 
 void Merge::seek (std::vector<Cluster> &clusters) {
-  //! Find clusters with members in common.
+  // Find clusters with members in common.
   for(int i = 0; i < clusters.size(); i++)
     if(clusters[i].mem.size() > 0)
       for(int j = 0; j < clusters.size(); j++)
@@ -41,16 +41,16 @@ void Merge::seek (std::vector<Cluster> &clusters) {
 }
 
 void Merge::destroy (std::vector<Cluster> &clusters) {
-  //! Function to erase empty Cluster instances.
+  // Function to erase empty Cluster instances.
   std::vector<int> remove_list;
-  /**< Loop through clusters */
+  /* Loop through clusters */
   for(int i = 0; i < clusters.size(); i++) {
-    /**< Check if cluster is empty */
+    /* Check if cluster is empty */
     if(clusters[i].mem.empty())
       remove_list.push_back(i);
   }
-  /**< Loop backwards through list of items to be removed */
+  /* Loop backwards through list of items to be removed */
   for(int i = remove_list.size(); i --> 0;)
-    /**< Erase empty cluster */
+    /* Erase empty cluster */
     clusters.erase(clusters.begin() + remove_list[i]);
 }
--- fof_cluster_finder/src/merge_class.hpp
+++ fof_cluster_finder/src/merge_class.hpp
@@ -1,25 +1,79 @@
-/*Class header for Galaxy*/
+/**
+ * @file merge_class.hpp
+ *
+ * @author Samuel Farrens
+ */
 
 #ifndef MERGE_CLASS_H
 #define MERGE_CLASS_H
+
+/**
+ * @class Merge
+ *
+ * @brief Class for merging Cluster instances. 
+ *
+ * This class merges Cluster instances that have common Galaxy 
+ * instances.
+ */
 
 #include <algorithm> 
 #include <vector>
 #include "cluster_class.hpp"
 
-class Merge { //! Class for merge functions
-private:
-  bool gal_in_clt (const Galaxy &, const Cluster &);
-  bool check_mem (const Cluster &, const Cluster &);
-  void assimilate (Cluster &, Cluster &);
-  void seek (std::vector<Cluster> &);
-  void destroy (std::vector<Cluster> &);
+class Merge { // Class for merge functions
+
 public:
+
+  /** 
+   * Initialise Merge instance.
+   * @param[in] clusters Vector of Cluster instances.
+   */
   Merge (std::vector<Cluster> &clusters) {
-    /**< Initialise Merge instance. */
     seek(clusters);
     destroy(clusters);
   };
+
+private:
+
+  /**
+   * This method checks if a given Galaxy instance is a member
+   * of a given Cluster instance.
+   * @param[in] gal Galaxy instance.
+   * @param[in] clt Cluster instance.
+   */
+  bool gal_in_clt (const Galaxy &, const Cluster &);
+
+  /**
+   * This method checks if two given Cluster instances have any
+   * common member Galaxy instances.
+   * @param[in] clt1 Cluster instance 1.
+   * @param[in] clt2 Cluster instance 2.
+   */
+  bool check_mem (const Cluster &, const Cluster &);
+
+  /**
+   * This method merges the member Galaxy instances from  
+   * Cluster instance 2 into Cluster instance 1 and removes
+   * duplicates.
+   * @param[in] clt1 Cluster instance 1.
+   * @param[in] clt2 Cluster instance 2.
+   */
+  void assimilate (Cluster &, Cluster &);
+
+  /**
+   * This method searches for Cluster instances with common
+   * members.
+   * @param[in] clusters Vector of Cluster instances.
+   */
+  void seek (std::vector<Cluster> &);
+
+  /**
+   * This method removes empty Cluster instances from the
+   the list.
+   * @param[in] clusters Vector of Cluster instances.
+   */
+  void destroy (std::vector<Cluster> &);
+
 };
 
-#endif /* MERGE_CLASS_H */
+#endif // MERGE_CLASS_H
