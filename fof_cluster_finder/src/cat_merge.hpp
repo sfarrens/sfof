c12cc98087260c57458b4b1fe7e1928b6be92a7a
sfarrens <samuel.farrens@googlemail.com>
2014-07-18 16:16:43 +0200

    Updated comments for class: Cat_Merge

--- fof_cluster_finder/src/cat_merge.hpp
+++ fof_cluster_finder/src/cat_merge.hpp
@@ -1,12 +1,24 @@
-/*Class header for Main*/
+/**
+ * @file cat_merge.hpp
+ *
+ * @author Samuel Farrens
+ */
 
 #ifndef CAT_MERGE_CLASS_H
 #define CAT_MERGE_CLASS_H
 
+/**
+ * @class Cat_Merge
+ *
+ * @brief Class for splitting galaxy catalogues.
+ *
+ * This class splits a catalogue of galaxies into overlapping 
+ * pieces.
+ */
+
 #include <iostream>
 #include <omp.h>
 #include <vector>
-
 #include "comp.hpp"
 #include "cluster_class.hpp"
 #include "cat_merge_fileio.hpp"
@@ -14,19 +26,57 @@
 #include "merge_class.hpp"
 #include "option_class.hpp"
 
-class Cat_Merge { //! Class structure for Cat_Merge.
-private:
-  Fileio fileio;
-  Option opt;
-  Merge_Fileio merge_fileio;
-  std::vector<Cluster> clusters;
+class Cat_Merge { // Class structure for Cat_Merge.
+
 public:
+
+  /// Include Comp class.
   Comp comp;
+
+  /**
+   * This method calls Option to read the code options from the 
+   * provided arguments.
+   * @param[in] argc Argument count.
+   * @param[in] argv Argument vector.
+   */
   void read_options (int, char *[]);
+
+  /**
+   * This method calls Merge_Fileio to read the input file and stores the 
+   * data in Galaxy instances.
+   */
   void read_files ();
+
+  /**
+   * This method initialises a Merge instance.
+   */
   void merge_clusters ();
+
+  /**
+   * This method assigns properties to the Cluster instances.
+   */
   void assign_cluster_props ();
+
+  /**
+   * This method calls Fileio to write the Cluster instances and 
+   * corresponding member Galaxy instances to files.
+   */
   void write_files ();
+  
+private:
+  
+  /// Include Fileio class.
+  Fileio fileio;
+  
+  /// Include Option class.
+  Option opt;
+  
+  /// Include Merge_Fileio class.
+  Merge_Fileio merge_fileio;
+ 
+  /// Vector of Cluster instances.
+  std::vector<Cluster> clusters;
+
 };
 
-#endif /* CAT_MERGE_CLASS_H */
+#endif // CAT_MERGE_CLASS_H
