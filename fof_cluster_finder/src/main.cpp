e62e7fdb685524b99140f2e7da135057bae0c602
sfarrens <samuel.farrens@googlemail.com>
2014-07-18 15:00:19 +0200

    Updated comments for class: Main

--- fof_cluster_finder/src/main.cpp
+++ fof_cluster_finder/src/main.cpp
@@ -3,7 +3,7 @@
 #include "main.hpp"
 
 void Main::read_options (int argc, char *argv[]) {
-  //! Function to read code options.
+  // Function to read code options.
   double version_number = 3.0;
   param_file = "param_file.ini";
   opt.read_param_file(param_file);
@@ -11,7 +11,7 @@ void Main::read_options (int argc, char *argv[]) {
 }
 
 void Main::read_data () {
-  //! Function to read input file.
+  // Function to read input file.
   if (opt.fof_mode == "spec")
     fileio.set_up(1, 2, 3, 4);
   else
@@ -25,7 +25,7 @@ void Main::read_data () {
 }
 
 void Main::set_up_zbins () {
-  //! Function to set up the redshift bins.
+  // Function to set up the redshift bins.
   num_bins = astro.num_bins(opt.z_min, opt.z_max, opt.z_bin_size);
   for(int i = 0; i < num_bins; i++){
     Zbin zbin(i, opt.z_min + i * opt.z_bin_size, opt.z_bin_size);
@@ -45,8 +45,8 @@ void Main::set_up_zbins () {
 }
 
 void Main::assign_linking_param () {
-  //! Function to assign linking parameter values to redshift bins.
-  //! A reference value is defined in order to ensure uniformity.
+  // Function to assign linking parameter values to redshift bins.
+  // A reference value is defined in order to ensure uniformity.
   if(opt.link_mode == "fixed")
     for(int i = 0; i < num_bins; i++)
       zbins[i].assign_fixed_rfriend(opt.link_r); 
@@ -70,13 +70,13 @@ void Main::assign_linking_param () {
 }
 
 void Main::make_kdtree () {
-  //! Function to split data into kd-tree.
+  // Function to split data into kd-tree.
   std::cout<<"Building kd-tree to depth of "<<opt.kdtree_depth<<std::endl;
   tree.set_kdtree(galaxies, opt.kdtree_depth);
 }
 
 void Main::find_friends () {
-  //! Function that implements the friends-of-friends.
+  // Function that implements the friends-of-friends.
   std::cout<<"Performing FoF in "<<opt.fof_mode<<" mode."<<std::endl;
   int nbins = num_bins, cluster_count = 0;
   if(opt.fof_mode == "spec") nbins = 1;
@@ -111,30 +111,30 @@ void Main::find_friends () {
 }
 
 void Main::merge_clusters () {
-  //! Function that merges clusters with members in common.
+  // Function that merges clusters with members in common.
   std::cout<<"Merging clusters."<<std::endl;
   Merge merge_clusters(clusters);
 }
 
 void Main::assign_cluster_props () {
-  //! Funciton that assigns cluster properties.
+  // Funciton that assigns cluster properties.
   for(int i = 0; i < clusters.size(); i++) {
-    /**< Remove duplicate members */
+    /* Remove duplicate members */
     clusters[i].unique();
-    /**< Assing properties */
+    /* Assing properties */
     clusters[i].assign_props();
     clusters[i].assign_sn(opt.bg_expect);
   }
-  /**< Sort clusters by number of members */
+  /* Sort clusters by number of members */
   std::sort(clusters.begin(), clusters.end());
   std::reverse(clusters.begin(), clusters.end());
-  //**< Rename clusters */
+  //* Rename clusters */
   for(int i = 0; i < clusters.size(); i++) 
     clusters[i].rename(i + 1);
 }
 
 void Main::output_results () {
-  //! Function that outputs detected clusters.
+  // Function that outputs detected clusters.
   std::string cluster_file_name, member_file_name;
   fileio.output_file_names(opt.input_file, opt.fof_mode, 
 			   opt.output_mode, opt.link_r, opt.link_z, 
--- fof_cluster_finder/src/main.hpp
+++ fof_cluster_finder/src/main.hpp
@@ -1,12 +1,24 @@
-/*Class header for Main*/
+/**
+ * @file main.hpp
+ *
+ * @author Samuel Farrens
+ */
 
 #ifndef MAIN_CLASS_H
 #define MAIN_CLASS_H
 
+/**
+ * @class Main
+ *
+ * @brief Class for running the FoF algorithm.
+ *
+ * This class runs the friends-of-friends cluster detection 
+ * algorithm.
+ */
+
 #include <iostream>
 #include <omp.h>
 #include <vector>
-
 #include "astro.hpp"
 #include "comp.hpp"
 #include "cluster_class.hpp"
@@ -18,28 +30,94 @@
 #include "option_class.hpp"
 #include "zbin_class.hpp"
 
-class Main { //! Class structure for Main.
-private:
-  Astro astro;
-  Fileio fileio;
-  Kdtree tree;
-  Option opt;
-  int num_bins;
-  std::string param_file;
-  std::vector<Zbin> zbins;
-  std::vector<Galaxy> galaxies;
-  std::vector<Cluster> clusters;
+class Main { // Class structure for Main.
+
 public:
+
+  /// Include Comp class.
   Comp comp;
+
+  /**
+   * This method calls Option to read the code options from the 
+   * provided arguments and/or the parameter file.
+   * @param[in] argc Argument count.
+   * @param[in] argv Argument vector.
+   */
   void read_options (int, char *[]);
+
+  /**
+   * This method calls Fileio to read the input file and stores the 
+   * data in Galaxy instances.
+   */
   void read_data ();
+
+  /**
+   * This method sets up redshift bins in Zbin instances.
+   */
   void set_up_zbins ();
+
+  /**
+   * This method assigns linking parameter values to Zbin instances. 
+   * A reference value is defined in order to ensure uniformity.
+   */
   void assign_linking_param ();
+
+  /**
+   * This method initialises a Kdtree instance.
+   */
   void make_kdtree ();
+
+  /**
+   * This method initialises a series of FoF instances for the 
+   * corresponding Zbin instances.
+   */
   void find_friends ();
+
+  /**
+   * This method initialises a Merge instance.
+   */
   void merge_clusters ();
+
+  /**
+   * This method assigns properties to the Cluster instances.
+   */
   void assign_cluster_props ();
+
+  /**
+   * This method calls Fileio to write the Cluster instances and 
+   * corresponding member Galaxy instances to files.
+   */
   void output_results ();
+
+private:
+
+  /// Include Astro class.
+  Astro astro;
+
+  /// Include Fileio class.
+  Fileio fileio;
+
+  /// Include Kdtree class.
+  Kdtree tree;
+
+  /// Include Option class.
+  Option opt;
+
+  /// Number of redshift bins.
+  int num_bins;
+
+  /// Option parameter file name.
+  std::string param_file;
+
+  /// Vector of Zbin instances.
+  std::vector<Zbin> zbins;
+
+  /// Vector of Galaxy instances.
+  std::vector<Galaxy> galaxies;
+
+  /// Vector of Cluster instances.
+  std::vector<Cluster> clusters;
+
 };
 
-#endif /* MAIN_CLASS_H */
+#endif // MAIN_CLASS_H
