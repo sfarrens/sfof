ed66d77e3d0490a53a28668e9f85082a0f9821e6
sfarrens <samuel.farrens@googlemail.com>
2014-07-18 13:53:28 +0200

    Updated comments for class: Zbin

--- fof_cluster_finder/src/zbin_class.cpp
+++ fof_cluster_finder/src/zbin_class.cpp
@@ -5,17 +5,17 @@
 #include "dh.h"
 
 void Zbin::assign_dist (double c, double H0, double Omega_M, double Omega_L) {
-  //! Calculate the angular diameter distance and the differential comoving 
-  //! volume element for a Zbin instance.
+  // Calculate the angular diameter distance and the differential comoving 
+  // volume element for a Zbin instance.
   dvdz = dcomvoldz(z, Omega_M, Omega_L);
   da = ((c / H0) * angdidis(z, Omega_M, Omega_L));
 }
 
 void Zbin::assign_rfriend (double r_ref) {
-  //! Scale linking length to redshift bin.
+  // Scale linking length to redshift bin.
   dndz = double(count) / dz;
   dndv = dndz / dvdz;
-  if(dndv > 0)
+  if(dndv > 0) 
     link_r = pow(dndv, -0.5) * r_ref;
   else
     link_r = 0.0;
@@ -23,7 +23,12 @@ void Zbin::assign_rfriend (double r_ref) {
 }
 
 void Zbin::assign_fixed_rfriend (double link_r_val) {
-  //! Assign fixed linking length value.
-  link_r = link_r_val;
+  // Assign fixed linking length value.
+  dndz = double(count) / dz;
+  dndv = dndz / dvdz;
+  if(dndv > 0)
+    link_r = link_r_val;
+  else
+    link_r = 0.0;
   rfriend = link_r / da;
 }
--- fof_cluster_finder/src/zbin_class.hpp
+++ fof_cluster_finder/src/zbin_class.hpp
@@ -1,12 +1,60 @@
-/*Class header for Zbin*/
+/**
+ * @file zbin_class.hpp
+ *
+ * @author Samuel Farrens
+ */
 
 #ifndef ZBIN_CLASS_H
 #define ZBIN_CLASS_H
 
-class Zbin { //! Class structure for redshift bin properties.
+/**
+ * @class Zbin
+ *
+ * @brief Class for storing redshift bin properties.
+ *
+ * This class calculates and stores Zbin properties.
+ */
+
+class Zbin { // Class structure for redshift bin properties.
+
 public:
-  int num, count;
-  double z, dz, da, dvdz, dndz, dndv, link_r, rfriend;
+
+  /// Number associated to Zbin instance.
+  int num;
+
+  /// Number of Galaxy instances corresponding to Zbin instance.
+  int count;
+
+  /// Redshift of Zbin instance.
+  double z;
+
+  /// Redshift bin size of Zbin instance.
+  double dz;
+
+  /// Angular diameter distance of Zbin instance.
+  double da;
+
+  /// Differential comoving volume of Zbin instance.
+  double dvdz;
+
+  /// Surface number density of Zbin instance.
+  double dndz;
+
+  /// Volume number density of Zbin instance.
+  double dndv;
+
+  /// Corrected transverse linking parameter value of Zbin instance.
+  double link_r;
+  
+  /// Transverse linking radius of Zbin instance.
+  double rfriend;
+
+  /** 
+   * Initialise Zbin instance.
+   * @param[in] num_val Integer value.
+   * @param[in] z_val Redshift.
+   * @param[in] z_bin_size Redshift bin size.
+   */
   Zbin(int num_val, double z_val, double z_bin_size) { 
     /**< Initialise Zbin instance. */
     num = num_val;
@@ -14,9 +62,31 @@ public:
     dz = z_bin_size;
     count = 0;
   };
+
+  /**
+   * This method calculates the angular diameter distance of a
+   * Zbin instance for a given cosmology.
+   * @param[in] c Speed of light [km/s].
+   * @param[in] H0 Hubble parameter [km/s/Mpc].
+   * @param[in] Omega_M Matter density.
+   * @param[in] Omega_L Dark energy density.
+   */
   void assign_dist (double, double, double, double);
+
+  /**
+   * This method scales transverse lining parameter value to the
+   * redshift of the Zbin instance.
+   * @param[in] r_ref Reference transverse linking parameter value.
+   */
   void assign_rfriend (double);
+
+  /**
+   * This method sets a fixed transverse lining parameter value for
+   * the Zbin instance.
+   * @param[in] link_r_val Transverse linking parameter value.
+   */
   void assign_fixed_rfriend (double);
+
 };
 
-#endif /* ZBIN_CLASS_H */
+#endif // ZBIN_CLASS_H
