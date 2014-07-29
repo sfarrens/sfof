823d623244e9bea85844c4e05a137d79539b9ae8
sfarrens <samuel.farrens@googlemail.com>
2014-07-29 16:23:09 +0200

    Exceptions output update

--- fof_cluster_finder/src/exceptions.cpp
+++ fof_cluster_finder/src/exceptions.cpp
@@ -10,8 +10,8 @@ std::invalid_argument(arg_name){
 BadArgumentExeception::BadArgumentExeception(const std::string& context,
         const std::string& arg_name, const std::string& expected):
 std::invalid_argument(arg_name){
-    message.append("BadArgumentExeception in").append(context).append(". ").append(arg_name).
-            append(" value. It must be ").append(expected);
+    message.append("BadArgumentExeception in ").append(context).append(", \"").append(arg_name).
+            append("\" value. Input value must be ").append(expected).append(".");
 }
 const char* BadArgumentExeception::what() const throw(){
     return message.c_str();
