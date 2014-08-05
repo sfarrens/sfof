f52724a88823914e5d463677869b7731892cb44f
sfarrens <samuel.farrens@googlemail.com>
2014-08-05 17:37:54 +0200

    cat_split bug fix

--- fof_cluster_finder/src/cat_split.cpp
+++ fof_cluster_finder/src/cat_split.cpp
@@ -38,9 +38,15 @@ void Cat_Split::set_up () {
       throw RuntimeException("Cat_Split::set_up", "opt.n_procs", "> 1");
     number_of_files = opt.n_procs;
     ra_bin_size = pow(((opt.ra_upper - opt.ra_lower) * (opt.dec_upper - opt.dec_lower)) / double(number_of_files), 0.5);
-    dec_bin_size = ra_bin_size;
-    opt.n_ra_bins = ((opt.ra_upper - opt.ra_lower) / ra_bin_size);
+    opt.n_ra_bins = int(ceil((opt.ra_upper - opt.ra_lower) / ra_bin_size));
+    ra_bin_size = ((opt.ra_upper - opt.ra_lower) / opt.n_ra_bins);
+    opt.n_dec_bins = int(double(number_of_files) / double(opt.n_ra_bins));
+    dec_bin_size = ((opt.dec_upper - opt.dec_lower) / opt.n_dec_bins);
   }
+  std::cout<<"Number of bins in RA: "<<opt.n_ra_bins<<std::endl;
+  std::cout<<"Number of bins in Dec: "<<opt.n_dec_bins<<std::endl;
+  std::cout<<"Bin size in RA: "<<ra_bin_size<<std::endl;
+  std::cout<<"Bin size in Dec: "<<dec_bin_size<<std::endl;
 }
 
 void Cat_Split::open_files () {
@@ -62,6 +68,7 @@ void Cat_Split::find_bin (double ra, double dec, std::vector<int> &bins) {
   double ra_bin_up = (fabs(ra - opt.ra_lower) / ra_bin_size);
   double dec_bin_low = (fabs(dec - opt.dec_lower) / dec_bin_size) - opt.dec_overlap / dec_bin_size;
   double dec_bin_up = (fabs(dec - opt.dec_lower) / dec_bin_size);
+
   bins.push_back(int(ra_bin_low) + int(dec_bin_low) * opt.n_ra_bins);
   bins.push_back(int(ra_bin_up) + int(dec_bin_low) * opt.n_ra_bins);
   bins.push_back(int(ra_bin_low) + int(dec_bin_up) * opt.n_ra_bins);
