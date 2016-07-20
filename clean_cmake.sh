f6c9b6e07593aadc2859ebafaae6ae92ba37a34b
Samuel Farrens <samuel.farrens@googlemail.com>
2016-07-20 16:38:04 +0200

    added clean cmake script

---
+++ clean_cmake.sh
@@ -0,0 +1,3 @@
+make clean
+rm -r CMakeCache.txt CMakeFiles/ Makefile cmake_install.cmake CTestTestfile.cmake Testing
+rm -r ./fof_cluster_finder/CMakeFiles/ ./fof_cluster_finder/Makefile ./fof_cluster_finder/cmake_install.cmake ./fof_cluster_finder/CTestTestfile.cmake
